d7aca626d6f1006d1b9d220c713aa9d6fc608e5b
Volkan Genç <gencvolkan@gmail.com>
2026-03-08 00:14:35 +0300

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PaginationWebAppDemo.Data;
+
+namespace PaginationWebAppDemo.Controllers
+{
+    public class ProductController 
+        : Controller
+    {
+
+        private readonly AppDbContext appDbContext;
+
+        public ProductController(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+
+        /* public async Task<IActionResult> Index(int page = 1)
+         {
+             int pageSize = 5;
+             var totalCount = await appDbContext.Products.CountAsync();
+
+             var products = await appDbContext
+                                 .Products
+                                 .Skip((page - 1) * pageSize)
+                                 .Take(pageSize)
+                                 .ToListAsync();
+
+             ViewBag.TotalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+             ViewBag.CurrentPage = page;
+
+             return View(products);
+         }*/
+
+        //Main View
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            int pageSize = 10;
+
+            var totalCount = await appDbContext.Products.CountAsync();
+
+            ViewBag.TotalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+            ViewBag.CurrentPage = page;
+
+            return View();
+        }
+
+        /// <summary>
+        /// Partial View / Ajax data
+        /// </summary>
+        /// <param name="page"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> GetProducts(int page = 1)
+        
+        {
+
+            int pageSize = 10;
+            var totalCount = await appDbContext.Products.CountAsync();
+
+            var products = await appDbContext
+                                .Products
+                                .OrderBy(x => x.Id)
+                                .Skip((page - 1) * pageSize)
+                                .Take(pageSize)
+                                .ToListAsync();
+
+            ViewBag.TotalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+            ViewBag.CurrentPage = page;
+
+            return PartialView("_ProductList", products);
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaginationWebAppDemo.Data
+{
+    public class AppDbContext
+        : DbContext
+    {
+        public DbSet<Product> Products { get; set; }    
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+        }
+    }
+}
